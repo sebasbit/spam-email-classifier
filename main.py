aa66ba9bda7fdd691dfdc1c237de16ce27dc7b75
sebasbit <sebastian.torres0426@gmail.com>
2025-11-22 22:34:21 -0500

    Crear estructura base del proyecto

---
+++ main.py
@@ -0,0 +1,10 @@
+from spam_email_classifier.controllers.app_controller import AppController
+
+
+def main():
+    app = AppController()
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ spam_email_classifier/controllers/app_controller.py
@@ -0,0 +1,3 @@
+class AppController:
+    def run(self) -> None:
+        print("Work in progress...")
